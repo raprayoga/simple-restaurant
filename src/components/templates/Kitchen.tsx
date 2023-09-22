d06f55b5096d565c60451d984185ebd1197e4e46
raprayoga <prayogarafie@gmail.com>
2023-09-22 16:31:49 +0700

    feat(table): manage table

--- src/components/templates/Kitchen.tsx
+++ src/components/templates/Kitchen.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import DefaultLayout from '@/components/layouts/DefaultLayout'
 import AddOrderForm from '@/components/modules/AddOrderForm'
 import OrderList from '@/components/modules/OrderList'
+import Table from '@/components/modules/Table'
 
-interface KitchenProps {
+export interface KitchenProps {
   table: number
   setTable: (value: number) => void
 }
@@ -15,6 +16,7 @@ export default function Kitchen({ table, setTable }: KitchenProps) {
       <div className="flex">
         <div className="min-h-[80vh] w-7/12 border-r-2 border-gray-shadow pr-5">
           <h2 className="mb-5 text-2xl font-bold text-primary">Meja</h2>
+          <Table table={table} setTable={setTable} />
         </div>
         <div className="w-5/12 pl-5">
           <h2 className="mb-5 text-2xl font-bold text-primary">Order</h2>
